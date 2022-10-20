6d155bb3b9287de5ea17885c91c16ff084aec0b9
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-10-21 03:00:32 +0800

    Mobile ordering app scrimba

---
+++ index.js
@@ -0,0 +1,64 @@
+import { menuArray } from './data.js'
+
+
+
+document.addEventListener('click', function (e) {
+    if (e.target.dataset.add) {
+        handleOrder(e.target.dataset.add)
+    }
+})
+let orderedArray = [];
+function handleOrder(tweetId) {
+
+
+    const targetTweetObj = menuArray.filter(function (tweet) {
+        return tweet.id === tweetId
+    })[0]
+
+  //  return targetTweetObj;
+  //  renderMenu();
+
+  //gawa new object here
+}
+
+
+function getMenuHtml() {
+    let menuHtml = ``
+
+    menuArray.forEach(function (menu) {
+        let ing = ``
+
+        menu.ingredients.forEach(function (content, index) {
+            console.log(index, content)
+
+            if (index !== menu.ingredients.length - 1) {
+                content += ", "
+            }
+            ing += content;
+        })
+
+        menuHtml += `
+        <p class="product-name" id=menu-${menu.id}>${menu.name}</p>
+        <p class="product-ingredients">${ing}</p>
+        <p class="product-price">$${menu.price}</p>
+        <i class="fa-thin fa-plus" data-add=${menu.id}></i>
+        `
+    })
+    return menuHtml;
+}
+
+function getTotalOrder() {
+
+}
+
+
+function renderMenu() {
+    document.getElementById('menu-here').innerHTML = getMenuHtml();
+    console.log(orderedArray);
+}
+
+function renderTotal() {
+   // document.getElementById('total-order').innerHTML = 
+}
+
+renderMenu();
