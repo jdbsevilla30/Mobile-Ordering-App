ef184eb8eb2d6c7642dd3337265b91c59dc8613c
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-10-22 02:29:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { menuArray } from './data.js'
 
 let order = []
-let uniqueOrder = []
+let price = []
 
 
 document.addEventListener('click', function (e) {
@@ -11,6 +11,15 @@ document.addEventListener('click', function (e) {
 
 })
 
+function handleOrder(itemId) {
+    let totalPricePerPiece = 0;
+    const targetItemId = menuArray.filter(function (item) {
+        return item.id === itemId;
+    })[0]
+    //   order.push(targetItemId.name += targetItemId.name);
+    price.push(targetItemId.price += price);
+    renderMenu();
+}
 
 
 
@@ -19,7 +28,6 @@ function getMenuHtml() {
 
     menuArray.forEach(function (menu) {
         let ing = ``
-
         menu.ingredients.forEach(function (content, index) {
             //     console.log(index, content)
 
@@ -34,7 +42,7 @@ function getMenuHtml() {
         <p class="product-ingredients">${ing}</p>
         <p class="product-price">$${menu.price}</p>
         <i class="fa-thin fa-plus" data-add=${menu.id}></i>
-        <p>${menu.order}</p>
+
       
         `
     })
@@ -42,12 +50,33 @@ function getMenuHtml() {
 }
 
 
+function getOrderHtml() {
+    let orderHtml = ``;
+    let sum = 0;
+    for (let i = 0; i < price.length; i += 1) {
+        sum += price[i];
+    }
+    orderHtml += `
+    <br>
+    <br>
+    <br>
+    Your order is
+    <p>Item: ${order}</p>
+    <p>${order.length}</p>
+    <p>${sum}</p>
+    <p></p>
+    `
+    return orderHtml;
+}
 
 
 function renderMenu() {
     document.getElementById('menu-here').innerHTML = getMenuHtml();
+    document.getElementById('total-order').innerHTML = getOrderHtml();
 
 }
+
+
 
 
 renderMenu();
