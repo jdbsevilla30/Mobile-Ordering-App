7825f7cceb8bd02873e17ac9afe02a8466559d5f
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-10-22 06:04:22 +0800

    1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { menuArray } from './data.js'
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
 let order = []
 let price = []
@@ -9,23 +10,68 @@ document.addEventListener('click', function (e) {
         handleOrder(e.target.dataset.add)
     }
 
+    else if (e.target.dataset.remove) {
+        handleRemove(e.target.dataset.remove);
+    }
+
+    renderMenu();
 })
+
+function handleRemove(itemId) {
+
+    const targetItemId = order.filter(function (item) {
+        return item.id === itemId;
+    })[0]
+    console.log(targetItemId.id);
+}
+
+function getOrderHtml() {
+    let orderHtml = ``;
+    let sum = 0;
+
+    orderHtml += `
+    <h1>Your order is</h1>
+    `
+    for (let i = 0; i < price.length; i += 1) {
+        sum += price[i];
+
+        orderHtml += `
+        <br>
+        <p>Item: ${order[i].name}</p>
+        <p>Price: $${order[i].price}</p>
+        <input type="button" data-remove=${order[i].id} value="remove">
+        <br>
+        `
+    }
+    orderHtml += `
+    <br>
+    <p>Total: $${sum}</p>`
+
+    return orderHtml;
+}
+
+
+function renderMenu() {
+    document.getElementById('menu-here').innerHTML = getMenuHtml();
+    document.getElementById('total-order').innerHTML = getOrderHtml();
+
+}
 
 function handleOrder(itemId) {
     let totalPricePerPiece = 0;
     const targetItemId = menuArray.filter(function (item) {
         return item.id === itemId;
     })[0]
-    //   order.push(targetItemId.name += targetItemId.name);
     order.push(
-
         {
             name: targetItemId.name,
-            price: targetItemId.price
+            price: targetItemId.price,
+            id: uuidv4()
         })
     price.push(targetItemId.price);
     renderMenu();
 }
+
 
 
 
@@ -35,8 +81,6 @@ function getMenuHtml() {
     menuArray.forEach(function (menu) {
         let ing = ``
         menu.ingredients.forEach(function (content, index) {
-            //     console.log(index, content)
-
             if (index !== menu.ingredients.length - 1) {
                 content += ", "
             }
@@ -55,39 +99,6 @@ function getMenuHtml() {
 }
 
 
-function getOrderHtml() {
-    let orderHtml = ``;
-    let sum = 0;
-
-    orderHtml += `
-    <h1>Your order is</h1>
-    `
-
-    for (let i = 0; i < price.length; i += 1) {
-        sum += price[i];
-
-        orderHtml += `
-        <p>Item: ${order[i].name}</p>
-        <p>Price: $${order[i].price}</p>
-        <p></p>
-
-        <p></p>
-        `
-
-    }
-    orderHtml += `
-    <br>
-    <p>Total: $${sum}</p>`
-  
-    return orderHtml;
-}
-
-
-function renderMenu() {
-    document.getElementById('menu-here').innerHTML = getMenuHtml();
-    document.getElementById('total-order').innerHTML = getOrderHtml();
-
-}
 
 
 
